fe9479f95eba3377bd11a7352723cfb987eba912
Joel Scoble <joel.scoble@outlook.com>
2014-08-24 18:53:40 -0500

    added implementation of fisher-yates using slice of interfaces

---
+++ fisheryates.go
@@ -0,0 +1,35 @@
+// Copyright 2014, Joel Scoble, all rights reserved.
+// Licensed under The MIT License. Please view the LICENSE file for more
+// information.
+//
+// fisheryates.go implements the Fisher-Yates shuffling algorithm
+package shuffler
+
+import (
+	"math/rand"
+	"time"
+)
+
+
+func init() {
+	SeedRandom()
+}
+
+// SeedRandom seeds random with current time, in nano.
+func SeedRandom() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
+
+// Takes a 'set', a slice of interfaces, and shuffles the set, returning a 
+// slice of interfaces.
+func FisherYates(set []interface{}) []interface{} {
+	l := len(set)
+	for i := 0; i < l; i++ {
+		r := rand.Intn(i + 1)
+		if i != r {
+			set[r], set[i] = set[i], set[r]
+		}
+	}
+
+	return set
+}
